87b2aff6b4876bef86f11a085298a6647f4bef69
NWaka1415 <yuuki.ohta.chintap@gmail.com>
2019-05-17 03:42:22 +0900

    ・GameSceneを追加 ・Player追加 ・各シーン管理スクリプト追加 ・素材追加

---
+++ Assets/C#Scripts/GameSceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneManager : MonoBehaviour
+{
+    private Camera _camera;
+    private float _leftCameraWorldPos;
+    private float _rightCameraWorldPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        _leftCameraWorldPos = _camera.ViewportToWorldPoint(new Vector3(0, 0)).x;
+        _rightCameraWorldPos = _camera.ViewportToWorldPoint(new Vector3(1, 1)).x;
+    }
+
+    public float LeftCameraWorldPos => _leftCameraWorldPos;
+
+    public float RightCameraWorldPos => _rightCameraWorldPos;
+}
---
+++ Assets/C#Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float _speed;
+    private GameSceneManager _gameSceneManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _speed = 0.1f;
+        _gameSceneManager = GameObject.Find("GameSceneManager").GetComponent<GameSceneManager>();
+        if (_gameSceneManager == null) Debug.LogError("Error!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x <= _gameSceneManager.LeftCameraWorldPos)
+        {
+            //左端
+            transform.position = new Vector3(_gameSceneManager.LeftCameraWorldPos, transform.position.y);
+        }
+
+        if (transform.position.x >= _gameSceneManager.RightCameraWorldPos)
+        {
+            //右端
+            transform.position = new Vector3(_gameSceneManager.RightCameraWorldPos, transform.position.y);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //左矢印キーが押されたら
+            transform.Translate(-_speed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //右矢印キーが押されたら
+            transform.Translate(_speed, 0, 0);
+        }
+    }
+}
